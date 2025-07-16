05ebcce5b3775192bb3b8404e696d0567ef6f821
leeuihyun <meta212121@naver.com>
2025-07-16 19:04:58 +0900

    init : initial commit

---
+++ src/test/java/com/example/momo/MomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.momo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
