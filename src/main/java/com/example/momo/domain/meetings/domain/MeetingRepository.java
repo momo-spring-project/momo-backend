ebb0e7325cde917c1b58e2890f9697cf53af6481
hojin <hojin915@naver.com>
2025-07-18 16:47:36 +0900

    feat: meeting 예외 양식 추가, meeting/meetingParticipant 구조 설정

--- src/main/java/com/example/momo/domain/meetings/domain/MeetingRepository.java
+++ src/main/java/com/example/momo/domain/meetings/domain/MeetingRepository.java
@@ -1,4 +1,7 @@
 package com.example.momo.domain.meetings.domain;
 
+import java.util.Optional;
+
 public interface MeetingRepository {
+	Optional<Meeting> findById(Long id);
 }
---
+++ src/main/java/com/example/momo/domain/meetings/presentation/dto/ParticipantAddRequestDto.java
@@ -0,0 +1,7 @@
+package com.example.momo.domain.meetings.presentation.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ParticipantAddRequestDto {
+}
