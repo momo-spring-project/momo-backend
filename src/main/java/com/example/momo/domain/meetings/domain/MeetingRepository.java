c1062a4b7f1aa7ed16709c6643d3c821326637c9
leeuihyun <meta212121@naver.com>
2025-07-21 19:21:31 +0900

    feat : Meeting 목록 조회 로직 수정 제목 포함 여부 모임 목록 조회 -> 동적 쿼리 적용 목록 조회

--- src/main/java/com/example/momo/domain/meetings/domain/MeetingRepository.java
+++ src/main/java/com/example/momo/domain/meetings/domain/MeetingRepository.java
@@ -1,9 +1,12 @@
 package com.example.momo.domain.meetings.domain;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import com.example.momo.domain.meetings.enums.MeetingStatus;
 
 public interface MeetingRepository {
 
@@ -11,7 +14,8 @@ public interface MeetingRepository {
 
 	Meeting save(Meeting meeting);
 
-	Page<Meeting> findAllByTitleContaining(String title, Pageable pageable);
+	Page<Meeting> findMeetings(String title, LocalDateTime meetingDate, MeetingStatus status,
+		Pageable pageable);
 
 	boolean existsById(Long id);
 }
