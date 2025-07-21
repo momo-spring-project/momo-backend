938824fe1e20b957c47ebbfa08d0deac1b99e266
kodongwon <dw9706@gmail.com>
2025-07-21 22:35:12 +0900

    fix: hibernate.LazyInitializationException 버그 수정

--- src/main/java/com/example/momo/domain/auth/repository/UserSocialRepository.java
+++ src/main/java/com/example/momo/domain/auth/repository/UserSocialRepository.java
@@ -1,9 +1,11 @@
 package com.example.momo.domain.auth.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.momo.domain.auth.entity.UserSocial;
 
 public interface UserSocialRepository extends JpaRepository<UserSocial, Long> {
+	@EntityGraph(attributePaths = "user")
 	UserSocial findByProviderId(String providerId);
 }
