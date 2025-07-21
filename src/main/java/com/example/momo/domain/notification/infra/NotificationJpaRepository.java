be9f24db2f38515f209f41d133817d239953f209
Zyooon <wcw7373@gmail.com>
2025-07-21 16:30:22 +0900

    fix : @EnableAsync 추가. JpaRepository 불필요한 로직 제거

--- src/main/java/com/example/momo/domain/notification/infra/NotificationJpaRepository.java
+++ src/main/java/com/example/momo/domain/notification/infra/NotificationJpaRepository.java
@@ -3,10 +3,9 @@ package com.example.momo.domain.notification.infra;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.example.momo.domain.notification.domain.Notification;
 
 public interface NotificationJpaRepository extends JpaRepository<Notification, Long> {
-	List<Notification> findAllByUserId(@Param("userId") Long userId);
+	List<Notification> findAllByUserId(Long userId);
 }
