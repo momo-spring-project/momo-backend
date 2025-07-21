a613447797921f4752c8d21934b88605f844e0ac
kodongwon <dw9706@gmail.com>
2025-07-22 00:04:50 +0900

    feat: 소셜 회원 회원탈퇴 구현
    - userSocial 연동 hard delete

--- src/main/java/com/example/momo/domain/auth/dto/WithdrawRequest.java
+++ src/main/java/com/example/momo/domain/auth/dto/WithdrawRequest.java
@@ -1,12 +1,10 @@
 package com.example.momo.domain.auth.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class WithdrawRequest {
-    @NotBlank
-    private String password;
+	private String password;
 }
--- src/main/java/com/example/momo/domain/auth/service/AuthService.java
+++ src/main/java/com/example/momo/domain/auth/service/AuthService.java
@@ -1,57 +1,78 @@
 package com.example.momo.domain.auth.service;
 
-import com.example.momo.domain.auth.dto.*;
-import com.example.momo.domain.user.domain.User;
-import com.example.momo.domain.user.exception.UserException;
-import com.example.momo.domain.user.infra.UserRepository;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.example.momo.domain.auth.dto.AuthUser;
+import com.example.momo.domain.auth.dto.LoginRequest;
+import com.example.momo.domain.auth.dto.LoginResponse;
+import com.example.momo.domain.auth.dto.RegisterRequest;
+import com.example.momo.domain.auth.dto.WithdrawRequest;
+import com.example.momo.domain.auth.entity.UserSocial;
+import com.example.momo.domain.auth.repository.UserSocialRepository;
+import com.example.momo.domain.user.domain.User;
+import com.example.momo.domain.user.exception.UserException;
+import com.example.momo.domain.user.infra.UserRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AuthService {
-    private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
+	private final UserSocialRepository userSocialRepository;
 
-    @Transactional
-    public void register(RegisterRequest request) {
+	@Transactional
+	public void register(RegisterRequest request) {
 
-        if (userRepository.existsByNickname(request.getNickname())) {
-            throw UserException.duplicateNickname();
-        }
+		if (userRepository.existsByNickname(request.getNickname())) {
+			throw UserException.duplicateNickname();
+		}
 
-        if (userRepository.existsByEmail(request.getEmail())) {
-            throw UserException.duplicateEmail();
-        }
-        User user = new User(
-                request.getNickname(),
-                request.getEmail(),
-                passwordEncoder.encode(request.getPassword()),
-                null,
-                request.getLatitude(),
-                request.getLongitude()
-        );
-        userRepository.save(user);
-    }
+		if (userRepository.existsByEmail(request.getEmail())) {
+			throw UserException.duplicateEmail();
+		}
+		User user = new User(
+			request.getNickname(),
+			request.getEmail(),
+			passwordEncoder.encode(request.getPassword()),
+			null,
+			request.getLatitude(),
+			request.getLongitude()
+		);
+		userRepository.save(user);
+	}
 
-    public LoginResponse login(LoginRequest request) {
-        User user = userRepository.findByEmailAndIsDeletedFalse(request.getEmail()).orElseThrow(UserException::userNotFound);
-        //  비밀번호 검증
-        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) throw UserException.passwordMismatch();
+	public LoginResponse login(LoginRequest request) {
+		User user = userRepository.findByEmailAndIsDeletedFalse(request.getEmail())
+			.orElseThrow(UserException::userNotFound);
+		//  비밀번호 검증
+		if (!passwordEncoder.matches(request.getPassword(), user.getPassword()))
+			throw UserException.passwordMismatch();
 
-        return new LoginResponse(user.getId(), user.getEmail(), user.getNickname());
-    }
+		return new LoginResponse(user.getId(), user.getEmail(), user.getNickname());
+	}
 
-    @Transactional
-    public void withdraw(WithdrawRequest request, AuthUser authUser) {
-        User user = userRepository.findByIdAndIsDeletedFalse(authUser.getId()).orElseThrow(UserException::userNotFound);
-        //  비밀번호 검증
-        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) throw UserException.passwordMismatch();
+	@Transactional
+	public void withdraw(WithdrawRequest request, AuthUser authUser) {
+		User user = userRepository.findByIdAndIsDeletedFalse(authUser.getId()).orElseThrow(UserException::userNotFound);
 
-        user.delete();
-    }
+		//  비밀번호 검증
+		if (user.getPassword() != null && !passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+			throw UserException.passwordMismatch();
+		}
+
+		// 유저는 soft delete
+		user.delete();
+
+		// 유저 소셜은 hard delete -> 연동이 끊기는 개념
+		List<UserSocial> allUserSocial = userSocialRepository.findAllByUser(user);
+		userSocialRepository.deleteAll(allUserSocial);
+	}
 
 }
