af56a37f651ae6b7d942364dd6ebd1b149ad7588
leeuihyun <meta212121@naver.com>
2025-07-22 16:50:35 +0900

    chore : swagger 의존성 추가 및 설정 - 25년도 최신 버전 중 사용자가 가장 많은 버전 선택 2.8.8 - 서비스와 분리를 위해 설정 파일만 작성

---
+++ src/main/java/com/example/momo/global/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.example.momo.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		SecurityScheme bearerAuth = new SecurityScheme()
+			.type(SecurityScheme.Type.HTTP)
+			.scheme("bearer")
+			.bearerFormat("JWT");
+
+		SecurityRequirement securityRequirement = new SecurityRequirement()
+			.addList("BearerAuth");
+
+		return new OpenAPI()
+			.components(new Components().addSecuritySchemes("BearerAuth", bearerAuth))
+			.addSecurityItem(securityRequirement);
+	}
+}
