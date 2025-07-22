f42fda630e7e6f01e6f390b77c2ead41301ab15d
kodongwon <dw9706@gmail.com>
2025-07-22 10:21:42 +0900

    fix: AuthService 중괄호 삭제

--- src/main/java/com/example/momo/domain/auth/service/AuthService.java
+++ src/main/java/com/example/momo/domain/auth/service/AuthService.java
@@ -1,6 +1,5 @@
 package com.example.momo.domain.auth.service;
 
-
 import java.util.List;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,7 +13,6 @@ import com.example.momo.domain.auth.dto.RegisterRequest;
 import com.example.momo.domain.auth.dto.WithdrawRequest;
 import com.example.momo.domain.auth.entity.UserSocial;
 import com.example.momo.domain.auth.repository.UserSocialRepository;
-
 import com.example.momo.domain.user.domain.User;
 import com.example.momo.domain.user.exception.UserErrorCode;
 import com.example.momo.domain.user.exception.UserException;
@@ -54,18 +52,17 @@ public class AuthService {
 	public LoginResponse login(LoginRequest request) {
 		User user = userRepository.findByEmailAndIsDeletedFalse(request.getEmail())
 			.orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
-    
+
 		//  비밀번호 검증
 		if (!passwordEncoder.matches(request.getPassword(), user.getPassword()))
 			throw new UserException(UserErrorCode.PASSWORD_MISMATCH);
-
 
 		return new LoginResponse(user.getId(), user.getEmail(), user.getNickname());
 	}
 
 	@Transactional
 	public void withdraw(WithdrawRequest request, AuthUser authUser) {
-    
+
 		User user = userRepository.findByIdAndIsDeletedFalse(authUser.getId())
 			.orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
 
@@ -80,8 +77,7 @@ public class AuthService {
 		// 유저 소셜은 hard delete -> 연동이 끊기는 개념
 		List<UserSocial> allUserSocial = userSocialRepository.findAllByUser(user);
 		userSocialRepository.deleteAll(allUserSocial);
-	}
-
 		user.delete();
 	}
+
 }
