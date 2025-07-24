01ceaafc1113143a5a31aa2440436d73966ee00e
Zyooon <wcw7373@gmail.com>
2025-07-24 11:16:21 +0900

    fix : MessageFormatUtil 구조 개선
    - messages.properties 로 메세지 문구 관리
    - MessageKey Enum 으로 관리

--- src/main/java/com/example/momo/domain/messegehub/application/MessageFormatUtil.java
+++ src/main/java/com/example/momo/domain/messegehub/application/MessageFormatUtil.java
@@ -1,28 +1,42 @@
 package com.example.momo.domain.messegehub.application;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+
+import com.example.momo.domain.messegehub.enums.MessageKey;
+
+import lombok.RequiredArgsConstructor;
 
 //직접적인 메세지 생성
 @Component
+@RequiredArgsConstructor
 public class MessageFormatUtil {
 
+	private final MessageSource messageSource;
+
 	public String buildCreateMessage(String categoryName) {
-		return String.format("관심 지역에 새로운 %s 모임이 추가되었습니다.", categoryName);
+		return getMessage(MessageKey.CREATE.key(), categoryName);
 	}
 
 	public String buildUpdateMessage(String meetingName) {
-		return String.format("%s 모임에 변경사항이 있습니다.", meetingName);
+		return getMessage(MessageKey.UPDATE.key(), meetingName);
 	}
 
 	public String buildDeleteMessage(String meetingName) {
-		return String.format("%s 모임이 취소 되었습니다.", meetingName);
+		return getMessage(MessageKey.DELETE.key(), meetingName);
 	}
 
 	public String buildJoinMessage(String participantNickname) {
-		return String.format("%s 님이 모임에 참석했습니다.", participantNickname);
+		return getMessage(MessageKey.JOIN.key(), participantNickname);
 	}
 
 	public String buildCancelMessage(String participantNickname) {
-		return String.format("%s 님이 모임 참석을 취소했습니다.", participantNickname);
+		return getMessage(MessageKey.CANCEL.key(), participantNickname);
+	}
+
+	private String getMessage(String code, Object... args) {
+		return messageSource.getMessage(code, args, Locale.getDefault());
 	}
 }
