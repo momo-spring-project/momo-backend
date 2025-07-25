9b563c542dad87b48edcc5f234cb16641124a2fa
kodongwon <dw9706@gmail.com>
2025-07-25 12:56:03 +0900

    fix: Security Config permitAll url 변경

--- src/main/java/com/example/momo/global/config/SecurityConfig.java
+++ src/main/java/com/example/momo/global/config/SecurityConfig.java
@@ -80,9 +80,9 @@ public class SecurityConfig {
 				.requestMatchers(HttpMethod.PATCH, "/api/v2/categories/**").hasRole("ADMIN")
 				.requestMatchers("/favicon.ico", "/css/**", "/js/**", "/images/**", "/.well-known/**").permitAll()
 				.requestMatchers(
-					"/api/v1/auth/register",
-					"/api/v1/auth/login",
-					"/api/v1/auth/reissue",
+					"/api/v2/users/register",
+					"/api/v2/auth/login",
+					"/api/v2/auth/reissue",
 					"/api/v2/categories/**"
 				).permitAll()
 				.requestMatchers(
