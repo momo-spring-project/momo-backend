89f20992a122f619b286964bda05872dd8c0be5e
zeroColaa <sinyoung556@gmail.com>
2025-08-05 16:52:32 +0900

    refactor(migration): payments 테이블 마이그레이션 변경

--- src/main/resources/db/migration/V9__Create_payments_table.sql
+++ src/main/resources/db/migration/V9__Create_payments_table.sql
@@ -1,16 +1,20 @@
 CREATE TABLE payments
 (
-    id                bigint                         NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    amount            int                            NOT NULL,
-    created_at        datetime     DEFAULT NULL,
-    meeting_id        bigint                         NOT NULL,
-    paid_at           datetime     DEFAULT NULL,
-    user_id           bigint                         NOT NULL,
-    version           bigint       DEFAULT NULL,
-    order_id          varchar(255) DEFAULT NULL,
-    payment_method    varchar(255)                   NOT NULL,
-    pg_transaction_id varchar(255) DEFAULT NULL,
-    status            enum ('COMPLETED', 'REFUNDED') NOT NULL,
+    id                BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    amount            INT          NOT NULL,
+    created_at        DATETIME     DEFAULT NULL,
+    meeting_id        BIGINT       NOT NULL,
+    paid_at           DATETIME     DEFAULT NULL,
+    failed_at         DATETIME     DEFAULT NULL,
+    canceled_at       DATETIME     DEFAULT NULL,
+    refunded_at       DATETIME     DEFAULT NULL,
+    user_id           BIGINT       NOT NULL,
+    version           BIGINT       DEFAULT NULL,
+    order_id          VARCHAR(255) DEFAULT NULL,
+    payment_method    VARCHAR(255) NOT NULL,
+    pg_transaction_id VARCHAR(255) DEFAULT NULL,
+    fail_reason       VARCHAR(255) DEFAULT NULL,
+    status ENUM('PENDING', 'COMPLETED', 'FAILED', 'CANCELED', 'REFUNDED', 'EXPIRED') NOT NULL,
 
     UNIQUE KEY uq_payment_meeting_user (meeting_id, user_id)
 );
