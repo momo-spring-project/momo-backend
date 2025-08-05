0f47397fefa1f6546c9704e3a87aff75c72fbc26
Mybread2 <ckwnsgh4@naver.com>
2025-08-05 15:04:33 +0900

    refactor: 불필요한 클래스 및 메서드 제거 - 지금 당장은 회원탈퇴에 대해서만 이벤트를 발행하면되고, 이벤트를 발행하게 되면 계속해서 exchange에 쌓이고 아웃박스 테이블에도 쌓이기 때문에 추후에 필요하다면 구현할 예정

--- src/main/java/com/example/momo/global/rabbitmq/constant/RoutingKeys.java
+++ src/main/java/com/example/momo/global/rabbitmq/constant/RoutingKeys.java
@@ -3,7 +3,4 @@ package com.example.momo.global.rabbitmq.constant;
 public class RoutingKeys {
 	// User 관련 routing key들
 	public static final String USER_WITHDRAWN = "user.withdrawn";
-	public static final String USER_REGISTERED = "user.registered";
-	public static final String USER_FOLLOWED = "user.followed";
-	public static final String USER_RATED = "user.rated";
 }
