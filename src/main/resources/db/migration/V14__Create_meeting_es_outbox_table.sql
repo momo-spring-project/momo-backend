5d32e7947ccffcbb66be6b74a1a411e07500717c
leeuihyun <meta212121@naver.com>
2025-08-06 18:28:51 +0900

    feat : event_type 칼럼 추가 (flyway)

--- src/main/resources/db/migration/V14__Create_meeting_es_outbox_table.sql
+++ src/main/resources/db/migration/V14__Create_meeting_es_outbox_table.sql
@@ -2,8 +2,9 @@
 CREATE TABLE meeting_elasticsearch_outbox
 (
     id           BIGINT AUTO_INCREMENT PRIMARY KEY,
-    meeting_id   BIGINT    NOT NULL,
-    published    BOOLEAN   NOT NULL DEFAULT FALSE,
-    published_at TIMESTAMP NULL,
-    created_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    meeting_id   BIGINT      NOT NULL,
+    event_type   VARCHAR(20) NOT NULL,
+    published    BOOLEAN     NOT NULL DEFAULT FALSE,
+    published_at TIMESTAMP   NULL,
+    created_at   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
