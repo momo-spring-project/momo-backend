860989832e605aecea930a1c5724d4f639b53c09
leeuihyun <meta212121@naver.com>
2025-08-06 18:27:06 +0900

    feat : 아웃박스 레포지토리 로직 작성

---
+++ src/main/java/com/example/momo/domain/meeting/infra/meeting/MeetingOutBoxRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.example.momo.domain.meeting.infra.meeting;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.momo.domain.meeting.domain.MeetingElasticsearchOutbox;
+import com.example.momo.domain.meeting.domain.MeetingOutboxRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class MeetingOutBoxRepositoryImpl implements MeetingOutboxRepository {
+
+	private final MeetingOutboxJpaRepository meetingOutboxJpaRepository;
+
+	@Override
+	public void save(MeetingElasticsearchOutbox outbox) {
+		meetingOutboxJpaRepository.save(outbox);
+	}
+
+	@Override
+	public List<MeetingElasticsearchOutbox> findUnpublishedEvents() {
+		return meetingOutboxJpaRepository.findByPublishedFalseOrderByCreatedAt();
+	}
+
+	@Override
+	public Optional<MeetingElasticsearchOutbox> findById(Long id) {
+		return meetingOutboxJpaRepository.findById(id);
+	}
+}
