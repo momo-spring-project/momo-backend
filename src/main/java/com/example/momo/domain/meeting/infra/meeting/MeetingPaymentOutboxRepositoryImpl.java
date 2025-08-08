5439b555781714ce9c83a3ccf3275eae714171ac
leeuihyun <meta212121@naver.com>
2025-08-08 19:32:56 +0900

    feat : Outbox 비즈니스 로직 구현

---
+++ src/main/java/com/example/momo/domain/meeting/infra/meeting/MeetingPaymentOutboxRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.example.momo.domain.meeting.infra.meeting;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.momo.domain.meeting.domain.MeetingPaymentOutbox;
+import com.example.momo.domain.meeting.domain.MeetingPaymentOutboxRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class MeetingPaymentOutboxRepositoryImpl implements MeetingPaymentOutboxRepository {
+
+	private final MeetingPaymentOutboxJpaRepository jpaRepository;
+
+	@Override
+	public void save(MeetingPaymentOutbox outbox) {
+		jpaRepository.save(outbox);
+	}
+
+	@Override
+	public List<MeetingPaymentOutbox> findUnpublishedEvents() {
+		return jpaRepository.findByPublishedFalseOrderByCreatedAt();
+	}
+
+	@Override
+	public Optional<MeetingPaymentOutbox> findById(Long id) {
+		return jpaRepository.findByMeetingId(id);
+	}
+}
