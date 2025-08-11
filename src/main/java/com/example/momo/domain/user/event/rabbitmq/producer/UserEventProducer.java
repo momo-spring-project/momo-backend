90f8fcc8e613dad1d3ff7f3be68352ed8a31038b
Mybread2 <ckwnsgh4@naver.com>
2025-08-11 13:52:56 +0900

    refactor: 가독성 좋게 띄어쓰기

--- src/main/java/com/example/momo/domain/user/event/rabbitmq/producer/UserEventProducer.java
+++ src/main/java/com/example/momo/domain/user/event/rabbitmq/producer/UserEventProducer.java
@@ -33,10 +33,12 @@ public class UserEventProducer {
 	 */
 	public void publishUserWithdrawn(Long userId, String email, String nickname) {
 		UserEventMessage.UserWithdrawnData data = new UserEventMessage.UserWithdrawnData(userId, email, nickname);
+
 		EventWrapper<UserEventMessage.UserWithdrawnData> eventWrapper = EventWrapper.of(
 			EventTypeNames.USER_WITHDRAWN,
 			data
 		);
+
 		String correlationId = "user-withdrawn-" + userId;
 
 		publishEvent(eventWrapper, RoutingKeys.USER_WITHDRAWN, correlationId);
@@ -50,12 +52,15 @@ public class UserEventProducer {
 	 * @param followerNickname 팔로우한 사용자 닉네임
 	 */
 	public void publishUserFollowed(Long followerId, Long followingId, String followerNickname) {
-		UserEventMessage.UserFollowedData data = new UserEventMessage.UserFollowedData(followerId, followingId,
-			followerNickname);
+		UserEventMessage.UserFollowedData data =
+			new UserEventMessage.UserFollowedData(followerId, followingId,
+				followerNickname);
+
 		EventWrapper<UserEventMessage.UserFollowedData> eventWrapper = EventWrapper.of(
 			EventTypeNames.USER_FOLLOWED,
 			data
 		);
+
 		String correlationId = "user-followed-" + followerId + "-" + followingId;
 
 		publishEvent(eventWrapper, RoutingKeys.USER_FOLLOWED, correlationId);
@@ -80,6 +85,7 @@ public class UserEventProducer {
 				eventWrapper,
 				correlationData                   // 발행 결과 추적용 ID
 			);
+
 			log.info("[User] 이벤트 발행 요청 완료 - eventType: {}, eventId: {}, correlationId: {}",
 				eventWrapper.type(), eventWrapper.uuId(), correlationId);
 
