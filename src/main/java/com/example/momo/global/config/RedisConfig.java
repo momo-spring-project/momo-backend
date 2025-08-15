6e35fd5529d769693d58b6b3e6b605739daef21e
Mybread2 <ckwnsgh4@naver.com>
2025-08-15 20:14:29 +0900

    fix: RedisConfig.java 수정 - 순환참조 문제 해결 및 default 포트로 연결하려는 문제 해결

--- src/main/java/com/example/momo/global/config/RedisConfig.java
+++ src/main/java/com/example/momo/global/config/RedisConfig.java
@@ -1,7 +1,9 @@
 package com.example.momo.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -16,10 +18,18 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 @Configuration
 public class RedisConfig {
 
+	@Value("${REDIS_HOST}")
+	private String redisHost;
+
+	@Value("${REDIS_PORT}")
+	private int redisPort;
+
 	@Bean
-	public LettuceConnectionFactory redisConnectionFactory(LettuceConnectionFactory redisConnectionFactory) {
-		redisConnectionFactory.setShareNativeConnection(false);
-		return redisConnectionFactory;
+	@Primary
+	public LettuceConnectionFactory redisConnectionFactory() {
+		LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, redisPort);
+		factory.setShareNativeConnection(false);
+		return factory;
 	}
 
 	@Bean
